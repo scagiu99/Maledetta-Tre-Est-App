6aeb15ff71388bced7a39362c3192ce8b1757585
Giulia Scarpa <131446648+scagiu99@users.noreply.github.com>
2024-06-24 19:15:31 +0200

    Delete www directory

--- www/js/DetailsOfficialPost.js
+++
@@ -1,6 +0,0 @@
-function onCreateDetailOfficialPostScreen(officialPost) {
-  showScreen("#DetailOfficialPostScreen");
-  $("#officialTitle").text(officialPost.authorName);
-  $("#description").text(officialPost.comment);
-  $("#timestamp").text(officialPost.datetime);
-}
